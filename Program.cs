1c548951c2d08c22187fa0c483dfbc575d3be979
diego_neri <diego@diegoneri.com>
2020-10-02 20:37:24 -0300

    Finaliza o exercício Soma2Numeros

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Soma2Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroUm, numeroDois, soma;
+
+            Console.WriteLine("Cálculo da soma entre dois números.");
+            Console.WriteLine();
+            
+            Console.Write("Digite o primeiro número: ");
+            numeroUm = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o segundo número: ");
+            numeroDois = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+
+            soma = numeroUm + numeroDois;
+
+            Console.WriteLine($"Soma: {soma}");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# Soma2Numeros
-[console: Soma2Numeros] Leia 2 números inteiros e exiba o resultado da soma
+# Exercício `Soma2Numeros`
+
+`[console: Soma2Numeros]` Leia 2 números inteiros e exiba o resultado da soma
+
+```
+Cálculo da soma entre dois números.
+
+Digite o primeiro número: 30
+Digite o segundo número: 40
+
+Soma: 70
+```
